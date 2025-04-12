1d4d0565195e692ff408ab3b33e8b2b8ea492410
Mehmetbeydiyceksiniz63 <149189769+Mehmetbeydiyceksiniz63@users.noreply.github.com>
2025-04-12 19:42:43 +0300

    Update sudoers.py

--- AlexaMusic/plugins/sudo/sudoers.py
+++ AlexaMusic/plugins/sudo/sudoers.py
@@ -1,13 +1,12 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from config import BANNED_USERS, MONGO_DB_URI, OWNER_ID
+from config import BANNED_USERS, MONGO_DB_URI, OWNER_ID, MUSIC_BOT_NAME
 from strings import get_command
 from AlexaMusic import app
-from AlexaMusic import SUDOERS
+from AlexaMusic.misc import SUDOERS
 from AlexaMusic.utils.database import add_sudo, remove_sudo
 from AlexaMusic.utils.decorators.language import language
-
 # Command
 ADDSUDO_COMMAND = get_command("ADDSUDO_COMMAND")
 DELSUDO_COMMAND = get_command("DELSUDO_COMMAND")
