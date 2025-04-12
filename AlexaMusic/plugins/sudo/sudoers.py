a95c89e975bf1b56b8b12f7ae87bd3a921eac3a1
Mehmetbeydiyceksiniz63 <149189769+Mehmetbeydiyceksiniz63@users.noreply.github.com>
2025-04-12 19:39:34 +0300

    Update sudoers.py

--- AlexaMusic/plugins/sudo/sudoers.py
+++ AlexaMusic/plugins/sudo/sudoers.py
@@ -1,22 +1,10 @@
-# Copyright (C) 2024 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
-
-""""
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2024 -present Team=Alexa <https://github.com/TheTeamAlexa>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
-
-
 from pyrogram import filters
 from pyrogram.types import Message
 
-from config import BANNED_USERS, MONGO_DB_URI, OWNER_ID, MUSIC_BOT_NAME
+from config import BANNED_USERS, MONGO_DB_URI, OWNER_ID
 from strings import get_command
 from AlexaMusic import app
-from AlexaMusic.misc import SUDOERS
+from AlexaMusic import SUDOERS
 from AlexaMusic.utils.database import add_sudo, remove_sudo
 from AlexaMusic.utils.decorators.language import language
 
@@ -26,54 +14,64 @@ DELSUDO_COMMAND = get_command("DELSUDO_COMMAND")
 SUDOUSERS_COMMAND = get_command("SUDOUSERS_COMMAND")
 
 
-@app.on_message(filters.command(ADDSUDO_COMMAND) & filters.user(OWNER_ID))
+@app.on_message(
+    filters.command(ADDSUDO_COMMAND) & filters.user(OWNER_ID)
+)
 @language
 async def useradd(client, message: Message, _):
     if MONGO_DB_URI is None:
         return await message.reply_text(
-            "**ᴅᴜᴇ ᴛᴏ {MUSIC_BOT_NAME}'s ᴩʀɪᴠᴀᴄʏ ɪssᴜᴇs, ʏᴏᴜ ᴄᴀɴ'ᴛ ᴍᴀɴᴀɢᴇ sᴜᴅᴏ ᴜsᴇʀs ᴏɴ {MUSIC_BOT_NAME} ᴅᴀᴛᴀʙᴀsᴇ.\n\n ᴩʟᴇᴀsᴇ ᴀᴅᴅ ʏᴏᴜʀ ᴍᴏɴɢᴏ ᴅᴀᴛᴀʙᴀsᴇ ɪɴ ᴠᴀʀs ᴛᴏ ᴜsᴇ ᴛʜɪs ғᴇᴀᴛᴜʀᴇ.**"
+            "**Botun gizlilik sorunları nedeniyle LostMuzik Veritabanını kullanırken sudo kullanıcılarını yönetemezsiniz.\n\ Bu özelliği kullanmak için lütfen vars'larınızda MONGO_DB_URI'nizi doldurun.**"
         )
     if not message.reply_to_message:
         if len(message.command) != 2:
-            return await message.reply_text(_["auth_1"])
+            return await message.reply_text(_["general_1"])
         user = message.text.split(None, 1)[1]
         if "@" in user:
             user = user.replace("@", "")
         user = await app.get_users(user)
         if user.id in SUDOERS:
-            return await message.reply_text(_["sudo_1"].format(user.mention))
+            return await message.reply_text(
+                _["sudo_1"].format(user.mention)
+            )
         added = await add_sudo(user.id)
         if added:
             SUDOERS.add(user.id)
             await message.reply_text(_["sudo_2"].format(user.mention))
         else:
-            await message.reply_text("ғᴀɪʟᴇᴅ.")
+            await message.reply_text("Failed")
         return
     if message.reply_to_message.from_user.id in SUDOERS:
         return await message.reply_text(
-            _["sudo_1"].format(message.reply_to_message.from_user.mention)
+            _["sudo_1"].format(
+                message.reply_to_message.from_user.mention
+            )
         )
     added = await add_sudo(message.reply_to_message.from_user.id)
     if added:
         SUDOERS.add(message.reply_to_message.from_user.id)
         await message.reply_text(
-            _["sudo_2"].format(message.reply_to_message.from_user.mention)
+            _["sudo_2"].format(
+                message.reply_to_message.from_user.mention
+            )
         )
     else:
-        await message.reply_text("ғᴀɪʟᴇᴅ.")
+        await message.reply_text("Failed")
     return
 
 
-@app.on_message(filters.command(DELSUDO_COMMAND) & filters.user(OWNER_ID))
+@app.on_message(
+    filters.command(DELSUDO_COMMAND) & filters.user(OWNER_ID)
+)
 @language
 async def userdel(client, message: Message, _):
     if MONGO_DB_URI is None:
         return await message.reply_text(
-            "**ᴅᴜᴇ ᴛᴏ {MUSIC_BOT_NAME}'s ᴩʀɪᴠᴀᴄʏ ɪssᴜᴇs, ʏᴏᴜ ᴄᴀɴ'ᴛ ᴍᴀɴᴀɢᴇ sᴜᴅᴏ ᴜsᴇʀs ᴏɴ {MUSIC_BOT_NAME} ᴅᴀᴛᴀʙᴀsᴇ.\n\n ᴩʟᴇᴀsᴇ ᴀᴅᴅ ʏᴏᴜʀ ᴍᴏɴɢᴏ ᴅᴀᴛᴀʙᴀsᴇ ɪɴ ᴠᴀʀs ᴛᴏ ᴜsᴇ ᴛʜɪs ғᴇᴀᴛᴜʀᴇ.**"
+            "**Botun gizlilik sorunları nedeniyle LostMuzik Veritabanını kullanırken sudo kullanıcılarını yönetemezsiniz.\n\n Bu özelliği kullanmak için lütfen vars'larınızda MONGO_DB_URI'nizi doldurun.**"
         )
     if not message.reply_to_message:
         if len(message.command) != 2:
-            return await message.reply_text(_["auth_1"])
+            return await message.reply_text(_["general_1"])
         user = message.text.split(None, 1)[1]
         if "@" in user:
             user = user.replace("@", "")
@@ -85,7 +83,7 @@ async def userdel(client, message: Message, _):
             SUDOERS.remove(user.id)
             await message.reply_text(_["sudo_4"])
             return
-        await message.reply_text(f"Something wrong happened.")
+        await message.reply_text(f"Yanlış bir şey oldu.")
         return
     user_id = message.reply_to_message.from_user.id
     if user_id not in SUDOERS:
@@ -95,28 +93,39 @@ async def userdel(client, message: Message, _):
         SUDOERS.remove(user_id)
         await message.reply_text(_["sudo_4"])
         return
-    await message.reply_text(f"sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ.")
+    await message.reply_text(f"Yanlış bir şey oldu.")
 
 
 @app.on_message(filters.command(SUDOUSERS_COMMAND) & ~BANNED_USERS)
 @language
 async def sudoers_list(client, message: Message, _):
     text = _["sudo_5"]
-    user = await app.get_users(OWNER_ID)
-    user = user.first_name if not user.mention else user.mention
-    text += f"1➤ {user}\n"
     count = 0
+    for x in OWNER_ID:
+        try:
+            user = await app.get_users(x)
+            user = (
+                user.first_name if not user.mention else user.mention
+            )
+            count += 1
+        except Exception:
+            continue
+        text += f"{count}» {user}\n"
     smex = 0
     for user_id in SUDOERS:
         if user_id not in OWNER_ID:
             try:
                 user = await app.get_users(user_id)
-                user = user.first_name if not user.mention else user.mention
+                user = (
+                    user.first_name
+                    if not user.mention
+                    else user.mention
+                )
                 if smex == 0:
                     smex += 1
                     text += _["sudo_6"]
                 count += 1
-                text += f"{count}➤ {user}\n"
+                text += f"{count}» {user}\n"
             except Exception:
                 continue
     if not text:
