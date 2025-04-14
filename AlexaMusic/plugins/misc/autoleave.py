7c1af1abe8dbeaa24338123bde7ddb7a239f759d
zerosugar6464 <musickumal@gmail.com>
2025-04-14 19:07:16 +0300

    Update autoleave.py

--- AlexaMusic/plugins/misc/autoleave.py
+++ AlexaMusic/plugins/misc/autoleave.py
@@ -1,14 +1,3 @@
-# Copyright (C) 2024 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
-
-""""
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2024 -present Team=Alexa <https://github.com/TheTeamAlexa>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
-
 import asyncio
 from pyrogram.enums import ChatType
 from datetime import datetime, timedelta
@@ -25,30 +14,32 @@ from AlexaMusic.utils.database import (
 
 autoend = {}
 
-
 async def auto_leave():
-    if config.AUTO_LEAVING_ASSISTANT == str(False):
-        while not await asyncio.sleep(config.AUTO_LEAVE_ASSISTANT_TIME):
-            from AlexaMusic.core.userbot import assistants
+    if config.AUTO_LEAVING_ASSISTANT:
+        while not await asyncio.sleep(900):
+            from AnonXMusic.core.userbot import assistants
 
             for num in assistants:
                 client = await get_client(num)
+                left = 0
                 try:
                     async for i in client.get_dialogs():
-                        chat_type = i.chat.type
-                        if chat_type in [
+                        if i.chat.type in [
                             ChatType.SUPERGROUP,
                             ChatType.GROUP,
                             ChatType.CHANNEL,
                         ]:
-                            chat_id = i.chat.id
                             if (
-                                chat_id != config.LOG_GROUP_ID
-                                and chat_id != -1002349741708
+                                i.chat.id != config.LOGGER_ID
+                                and i.chat.id != -1001686672798
+                                and i.chat.id != -1001549206010
                             ):
-                                if not await is_active_chat(chat_id):
+                                if left == 20:
+                                    continue
+                                if not await is_active_chat(i.chat.id):
                                     try:
-                                        await client.leave_chat(chat_id)
+                                        await client.leave_chat(i.chat.id)
+                                        left += 1
                                     except:
                                         continue
                 except:
@@ -59,32 +50,30 @@ asyncio.create_task(auto_leave())
 
 
 async def auto_end():
-    while True:
-        await asyncio.sleep(30)
-        for chat_id, timer in list(autoend.items()):
+    while not await asyncio.sleep(5):
+        ender = await is_autoend()
+        if not ender:
+            continue
+        for chat_id in autoend:
+            timer = autoend.get(chat_id)
+            if not timer:
+                continue
             if datetime.now() > timer:
                 if not await is_active_chat(chat_id):
-                    del autoend[chat_id]
+                    autoend[chat_id] = {}
                     continue
-                userbot = await get_assistant(chat_id)
-                members = []
-                async for member in userbot.get_call_members(chat_id):
-                    if member is None:
-                        continue
-                    members.append(member)
-                if len(members) <= 1:
-                    try:
-                        await Alexa.stop_stream(chat_id)
-                    except Exception:
-                        pass
-                    try:
-                        await app.send_message(
-                            chat_id,
-                            "ʙᴏᴛ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ᴄʟᴇᴀʀᴇᴅ ᴛʜᴇ ǫᴜᴇᴜᴇ ᴀɴᴅ ʟᴇғᴛ ᴠɪᴅᴇᴏᴄʜᴀᴛ ʙᴇᴄᴀᴜsᴇ ɴᴏ ᴏɴᴇ ᴡᴀs ʟɪsᴛᴇɴɪɴɢ sᴏɴɢs ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
-                        )
-                    except Exception:
-                        pass
-                del autoend[chat_id]
+                autoend[chat_id] = {}
+                try:
+                    await Anony.stop_stream(chat_id)
+                except:
+                    continue
+                try:
+                    await app.send_message(
+                        chat_id,
+                        "» ʙᴏᴛ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ʟᴇғᴛ ᴠɪᴅᴇᴏᴄʜᴀᴛ ʙᴇᴄᴀᴜsᴇ ɴᴏ ᴏɴᴇ ᴡᴀs ʟɪsᴛᴇɴɪɴɢ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
+                    )
+                except:
+                    continue
 
 
 asyncio.create_task(auto_end())
