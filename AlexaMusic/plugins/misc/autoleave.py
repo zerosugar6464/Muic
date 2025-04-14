f22610fcaa521c40585da5be455fc4582fcfeeda
zerosugar6464 <musickumal@gmail.com>
2025-04-14 13:18:01 +0300

    Update autoleave.py

---
+++ AlexaMusic/plugins/misc/autoleave.py
@@ -0,0 +1,80 @@
+import asyncio
+from datetime import datetime
+
+import config
+from pyrogram.enums import ChatType
+from AlexaMusic import app
+from AlexaMusic.core.call import AlexaMusic, autoend
+from AlexaMusic.utils.database import (get_client, is_active_chat,
+                                       is_autoend)
+
+
+async def auto_leave():
+    if config.AUTO_LEAVING_ASSISTANT == str(True):
+        while not await asyncio.sleep(
+            config.AUTO_LEAVE_ASSISTANT_TIME
+        ):
+            from LostMuzik.core.userbot import assistants
+
+            for num in assistants:
+                client = await get_client(num)
+                left = 0
+                try:
+                    async for i in client.iter_dialogs():
+                        chat_type = i.chat.type
+                        if chat_type in [
+                            ChatType.SUPERGROUP,
+                            ChatType.GROUP,
+                            ChatType.CHANNEL,
+                        ]:
+                            chat_id = i.chat.id
+                            if (
+                                chat_id != config.LOG_GROUP_ID
+                                and chat_id != -1002235971433
+                                and chat_id != -1002235971433
+                                and chat_id != -1002235971433
+                            ):
+                                if left == 20:
+                                    continue
+                                if not await is_active_chat(chat_id):
+                                    try:
+                                        await client.leave_chat(
+                                            chat_id
+                                        )
+                                        left += 1
+                                    except:
+                                        continue
+                except:
+                    pass
+
+
+asyncio.create_task(auto_leave())
+
+
+async def auto_end():
+    while not await asyncio.sleep(5):
+        if not await is_autoend():
+            continue
+        for chat_id in autoend:
+            timer = autoend.get(chat_id)
+            if not timer:
+                continue
+            if datetime.now() > timer:
+                if not await is_active_chat(chat_id):
+                    autoend[chat_id] = {}
+                    continue
+                autoend[chat_id] = {}
+                try:
+                    await LostMuzik.stop_stream(chat_id)
+                except:
+                    continue
+                try:
+                    await app.send_message(
+                        chat_id,
+                        "**Sesli Sohbette Kimse Olmadığı İçin Bot Otomatik Olarak Sesli Sohbetten Ayrıldı‌‌.** 🔮",
+                    )
+                except:
+                    continue
+
+
+asyncio.create_task(auto_end())
