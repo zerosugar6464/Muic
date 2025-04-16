91eb5a027c892ecb192f049bbbe5d32b292fc6fa
zerosugar6464 <musickumal@gmail.com>
2025-04-16 21:31:57 +0300

    Update logger.py

--- AlexaMusic/utils/logger.py
+++ AlexaMusic/utils/logger.py
@@ -1,3 +1,5 @@
+
+
 from config import LOG, LOG_GROUP_ID
 import psutil
 import time
@@ -11,27 +13,51 @@ from AlexaMusic.utils.database import (get_global_tops,
                                        get_served_users, get_sudoers,
                                        get_top_chats, get_topp_users)
 
-async def send_play_log(message, song_name: str):
-    user = message.from_user
-    chat = message.chat
 
-    chat_link = f"@{chat.username}" if chat.username else "Özel Grup"
 
-    log_text = f"""
-**Yeni Müzik Oynatıldı**
+async def play_logs(message, streamtype):
+    chat_id = message.chat.id
+    sayı = await app.get_chat_members_count(chat_id)
+    toplamgrup = len(await get_served_chats())
+    aktifseslisayısı = len(await get_active_chats())
+    aktifvideosayısı = len(await get_active_video_chats())
+    cpu = psutil.cpu_percent(interval=0.5)
+    mem = psutil.virtual_memory().percent
+    disk = psutil.disk_usage("/").percent
+    CPU = f"{cpu}%"
+    RAM = f"{mem}%"
+    DISK = f"{disk}%"
 
-👤 **Kullanıcı:** {user.mention}
-🔢 **ID:** `{user.id}`
-🏷️ **Kullanıcı Adı:** @{user.username or "Yok"}
 
-📌 **Grup:** {chat.title}
-🆔 **Grup ID:** `{chat.id}`
-🔗 **Grup Linki:** {chat_link}
+    if await is_on_off(LOG):
+        if message.chat.username:
+            chatusername = f"@{message.chat.username}"
+        else:
+            chatusername = "Gizli Grup 🔏"
+        logger_text = f"""
 
-🎶 **Parça:** `{song_name}`
-"""
 
-    try:
-        await app.send_message(LOG_GROUP_ID, log_text)
-    except Exception as e:
-        print(f"Log mesajı gönderilemedi: {e}")
+📌 **Grup :** {message.chat.title} [`{message.chat.id}`]
+👥 **Üye Sayısı : {sayı}**
+👤 **Kullanıcı :** {message.from_user.mention}
+✏️ **Kullanıcı Adı :** @{message.from_user.username}
+🔢 **Kullanıcı ID :** `{message.from_user.id}`
+🔗 **Grup Linki :** {chatusername}
+🔎 **Sorgu :** {message.text}
+
+**CPU :** {CPU}  ♨️  **RAM :** {RAM}  📂  **DISK :** {DISK}
+
+**Toplam Grup Sayısı : 👉{toplamgrup}**
+
+**Aktif Ses : {aktifseslisayısı}  🌬️  Aktif Video : {aktifvideosayısı}**"""
+        if message.chat.id != LOG_GROUP_ID:
+            try:
+                await app.send_message(
+                    LOG_GROUP_ID,
+                    f"{logger_text}",
+                    disable_web_page_preview=True,
+                )
+                await app.set_chat_title(LOG_GROUP_ID, f"AKTİF SES - {aktifseslisayısı}")
+            except:
+                pass
+        return
